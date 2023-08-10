b3e64be449f778cc061108c8341a6d8d88602dac
Roman Resenchuk <r@resrom.com>
2023-08-10 17:35:12 +0700

    Initial Commit

---
+++ WiseCurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WiseCurrencyConverter
+//
+//  Created by Roman Resenchuk on 10-08-2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
